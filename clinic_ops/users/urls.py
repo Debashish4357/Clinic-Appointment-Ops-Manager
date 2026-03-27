fce1cb59b1e420794813df3e3d1a994aa1603f3f
Debashish4357 <YourEmail>
2026-03-27 23:36:36 +0530

    Add Django backend Auth, CORS and Registration APIs

--- clinic_ops/users/urls.py
+++ clinic_ops/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import CustomTokenObtainPairView
+from .views import CustomTokenObtainPairView, register_user
 
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', register_user, name='register_user'),
 ]
